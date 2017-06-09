6b06728a7619c4f598d1975388a2ee0386789c70
micho10 <gatin13@yahoo.com>
2017-06-09 19:02:34 +0900

    Linear interpolation implementation WIP

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -14,22 +14,16 @@ object Visualization {
     * @param location Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
-  def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
-    //TODO Consider the special case of small distance
-    /*
-      - calculate collection distances between the known points and the new location
-        - case Some (dist <= 1 km) => use temperature of the closest point
-        - case None                => idw()
-     */
+  def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = temperatures.find(_._1 == location) match {
+//TODO Consider the special case of small distance
+//  - calculate collection distances between the known points and the new location
+//  - case Some (dist <= 1 km) => use temperature of the closest point
+//  - case None                => idw()
 
-    val temperature = temperatures.find(_._1 == location) match {
-      case Some(loc) => loc._2
-      case None =>
-        val weightedLocations = weightKnownLocations(temperatures, location, power = 3)(distance)
-        idw(weightedLocations)
-    }
-
-    temperature
+    case Some(loc) => loc._2
+    case None =>
+      val weightedLocations = weightKnownLocations(temperatures, location, power = 3)(distance)
+      idw(weightedLocations)
   }
 
   /**
@@ -37,14 +31,11 @@ object Visualization {
     * @param value The value to interpolate
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
-  def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = {
-    points.find(_._1 == value) match {
-      case Some((_, color)) => color
-      case None             => {
-        val (smaller, greater) = points.toList.sortBy(_._1).partition(_._1 < value)
-        linearInterpolation(smaller.tail, greater.head)
-      }
-    }
+  def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = points.find(_._1 == value) match {
+    case Some((_, color)) => color
+    case None             =>
+      val (smaller, greater) = points.toList.sortBy(_._1).partition(_._1 < value)
+      linearInterpolation(smaller.takeRight(1).headOption, greater.headOption, value)
   }
 
   /**
@@ -93,6 +84,7 @@ object Visualization {
   }
 
   /**
+    * Calculate the weights of the known locations
     *
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
     * @param location     Location where to predict the temperature
@@ -108,10 +100,48 @@ object Visualization {
     temperatures.map(temp => (temp._1, temp._2, weight(location, temp._1)))
   }
 
-  private def linearInterpolation(smaller.tail, greater.head): Color {
+  /**
+    * Approximates a color using linear interpolation between the colors of 2 known values
+    *
+    * @param p First known point
+    * @param q Second known point
+    * @return  The color inferred for the new value
+    */
+  private def linearInterpolation(p: Option[(Double, Color)], q: Option[(Double, Color)], value: Double): Color = {
+    def linearInterpolator(p: (Double, Color), q: (Double, Color)): Color = ???
 
+    (p, q) match {
+      case (Some(p), Some(q))             => linearInterpolator(p, q)
+      case (Some((pValue, pColor)), None) => pColor
+      case (None, Some((qValue, qColor))) => qColor
+      case _                              => Color(0, 0, 0)
+    }
   }
-
 
 }
 
+//https://stackoverflow.com/questions/4353525/floating-point-linear-interpolation
+//http://www.scala-notes.org/2010/08/a-generic-interpolate-method-using-type-classes/
+//http://zuqqhi2.com/en/linear-interpolation
+
+//private def interpolate(index: Int, x: T): T = {
+//  /* Interpolate or extrapolate linearly between point number index-1 and index. */
+//  assert(index > 0)
+//
+//  val x1 = X(index-1)
+//  val x2 = X(index)
+//  val y1 = Y(index-1)
+//  val y2 = Y(index)
+//  val f = implicitly[Field[T]]
+//
+//  // w = (x - x1) / (x2 - x1)
+//  val w = f./(f.-(x, x1),
+//  f.-(x2, x1))
+//  // u = 1 - w
+//  val u = f.-(f.one, w)
+//
+//  // result = y1 * u + y2 * w
+//  f.+(f.*(y1, u),
+//  f.*(y2, w))
+//}
+//}
