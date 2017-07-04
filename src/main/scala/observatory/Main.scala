054c2414f971d3c717e2059f52dcaf98f2da33fa
micho10 <gatin13@yahoo.com>
2017-07-04 17:00:21 +0900

    Started development of main to run the application

--- src/main/scala/observatory/Main.scala
+++ src/main/scala/observatory/Main.scala
@@ -1,5 +1,42 @@
 package observatory
 
+import java.io.File
+import java.time.LocalDate
+
 object Main extends App {
+
+  val stationsFile = "/stations.csv"
+
+
+  def extractData(year: Int, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Double)] = {
+    println(s"Extracting located temperatures of $year")
+    val temperatures = Extraction.locateTemperatures(year, stationsFile, temperaturesFile)
+    println(s"Located [$year]: ${temperatures.size}")
+    writeResults(year, temperatures)
+    temperatures
+  }
+
+  def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit): Unit = {
+    val p = new java.io.PrintWriter(f)
+    try { op(p) } finally { p.close() }
+  }
+
+  def writeResults(year: Int, temps: Iterable[(LocalDate, Location, Double)]): Unit =
+    printToFile(new File(s"output/located-$year.csv")) {p => temps.foreach(p.println)}
+
+
+  println("######## Observatory started ########")
+
+  // Extract localized temperatures
+//  val locatedTemps = (1975 to 1976).map(year => extractData(year, stationsFile, s"/$year.csv")).head
+  (1975 to 1985).par.map(year => extractData(year, stationsFile, s"/$year.csv"))
+
+//  // Calculate yearly average
+//  println("Calculating the yearly average temperatures")
+//  val yearlyAverageTemps = Extraction.locationYearlyAverageRecords(temperatures)
+//  println(s"Averages: ${yearlyAverageTemps.size}")
+
+
+  println("######## Observatory finished ########")
 
 }
--- src/test/scala/observatory/ExtractionTest.scala
+++ src/test/scala/observatory/ExtractionTest.scala
@@ -24,21 +24,21 @@ class ExtractionTest extends FunSuite {
   test("Extract the stations information from the stations file") {
     stations.show
     stations.filter((station: Station) => station.id == "035963").show
-    assertResult(14)(stations.count())
+    assertResult(15)(stations.count())
     assertResult(0)(stations.filter((station: Station) => station.id == "012345").count())
     assertResult(1)(stations.filter((station: Station) => station.id == "035963").count())
     assertResult(1)(stations.filter((station: Station) => station.id == "035963-35048").count())
-    assertResult(1)(stations.filter((station: Station) => station.id == "35048").count())
+    assertResult(1)(stations.filter((station: Station) => station.id == "-00004").count())
   }
 
   test("Extract the temperature information from the temperatures file") {
     temperatures.show(40)
     temperatures.filter((reading: TempReading) => reading.id == "049999").show
-    assertResult(30)(temperatures.count())
+    assertResult(31)(temperatures.count())
     assertResult(0)(temperatures.filter((reading: TempReading) => reading.id == "012345").count())
-    assertResult(16)(temperatures.filter((reading: TempReading) => reading.id == "049999").count())
+    assertResult(0)(temperatures.filter((reading: TempReading) => reading.id == "049999").count())
     assertResult(3)(temperatures.filter((reading: TempReading) => reading.id == "049999-00003").count())
-    assertResult(3)(temperatures.filter((reading: TempReading) => reading.id == "00003").count())
+    assertResult(1)(temperatures.filter((reading: TempReading) => reading.id == "-00008").count())
     assertResult(1)(temperatures.filter((reading: TempReading) => reading.id == "049999-00003"
       && reading.month == 10
       && reading.day == 3
@@ -53,7 +53,7 @@ class ExtractionTest extends FunSuite {
   test("Calculate yearly average for every location") {
     yearlyAverage foreach println
     assertResult(13)(yearlyAverage.size)
-    assertResult(0.5555555555555556)(yearlyAverage.find(_._1 == Location(72.58,-38.46)).)
+    assertResult(0.5555555555555556)(yearlyAverage.filter(_._1 == Location(72.58, -38.46)).head._2)
   }
 
 }
